78d6e74a196452cee8821968d35bcdf5e5071bf3
Roxy Pérez <necropsia32@gmail.com>
2025-06-17 19:25:27 +0100

    Initial commit

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Roxy Pérez  on 17/6/25.
+//
+
+import Foundation
---
+++ ExpenseTracker/TransactionModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionModel.swift
+//  ExpenseTracker
+//
+//  Created by Roxy Pérez  on 17/6/25.
+//
+
+import Foundation
